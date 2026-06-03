02252f5f17bd9b983b8ea651e8a6240ac302874c
Satyammisu <disysindia258@gmail.com>
2026-06-03 22:33:38 +0530

    Update Voice.js

--- js/Voice.js
+++ js/Voice.js
@@ -1,11 +1,32 @@
-function startVoiceInput() {
+function speakAnswer(text){
+
+    speechSynthesis.cancel();
+
+    const msg =
+    new SpeechSynthesisUtterance(text);
+
+    msg.rate = 1;
+
+    msg.pitch = 1;
+
+    msg.volume = 1;
+
+    speechSynthesis.speak(msg);
+
+}
+
+function startVoiceInput(){
 
     const SpeechRecognition =
         window.SpeechRecognition ||
         window.webkitSpeechRecognition;
 
-    if (!SpeechRecognition) {
-        alert("Voice recognition not supported in this browser.");
+    if(!SpeechRecognition){
+
+        alert(
+            "Voice recognition not supported."
+        );
+
         return;
     }
 
@@ -16,15 +37,18 @@ function startVoiceInput() {
 
     recognition.start();
 
-    recognition.onresult = function(event) {
+    recognition.onresult =
+    function(event){
 
         const text =
-            event.results[0][0].transcript;
+        event.results[0][0].transcript;
 
-        document.getElementById("question").value =
-            text;
+        document.getElementById(
+            "question"
+        ).value = text;
 
         askMangoMitra();
+
     };
 
 }
